7408a3edfd963661da6daad98f32ba087399dfef
Darryl Alang <daa59@uclive.ac.nz>
2023-08-16 22:17:10 +1200

    Fix translation and move composables to new files

--- app/src/main/java/nz/ac/uclive/daa59/topom/MainActivity.kt
+++ app/src/main/java/nz/ac/uclive/daa59/topom/MainActivity.kt
@@ -3,33 +3,21 @@ package nz.ac.uclive.daa59.topom
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
@@ -37,7 +25,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.delay
 import nz.ac.uclive.daa59.topom.ui.theme.ToPomTheme
 
 class MainActivity : ComponentActivity() {
@@ -81,8 +68,12 @@ fun BottomNav(navController: NavController) {
         items.forEach { item ->
             BottomNavigationItem(
                 icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
-                label = { Text(text = item.title,
-                    fontSize = 9.sp) },
+                label = { Text(text = when (item.title) {
+                    "todo" -> stringResource(id = R.string.to_do)
+                    "settings" -> stringResource(id = R.string.settings)
+                    "timer" -> stringResource(id = R.string.timer)
+                    else -> {""}},
+                fontSize = 9.sp) },
                 selectedContentColor = Color.Black,
                 unselectedContentColor = Color.Black.copy(0.4f),
                 alwaysShowLabel = true,
@@ -106,9 +97,9 @@ fun BottomNav(navController: NavController) {
 
 sealed class BottomNavItem(var title:String, var icon:Int, var screen_route:String){
 
-    object ToDo : BottomNavItem(R.string.to_do.toString(), R.drawable.icons8_list,"to_do")
-    object Timer: BottomNavItem(R.string.timer.toString(),R.drawable.icons8_timer,"timer")
-    object Settings: BottomNavItem(R.string.settings.toString(),R.drawable.icons8_settings,"settingd")
+    object ToDo : BottomNavItem("todo", R.drawable.icons8_list,"to_do")
+    object Timer: BottomNavItem("timer",R.drawable.icons8_timer,"timer")
+    object Settings: BottomNavItem("settings",R.drawable.icons8_settings,"settings")
 }
 
 @Composable
@@ -123,84 +114,5 @@ fun NavigationGraph(navController: NavHostController) {
         composable(BottomNavItem.Settings.screen_route) {
             SettingsScreen()
         }
-    }
-}
-
-@Composable
-fun TimerScreen(
-    totalTime: Long
-) {
-    var value by remember { mutableStateOf(0f) }
-    var currentTime by remember { mutableStateOf(totalTime) }
-    var currentSec by remember { mutableStateOf(0L) }
-    var currentMin by remember { mutableStateOf(25L) }
-    var isTimerOn by remember { mutableStateOf(false) }
-
-    LaunchedEffect(key1 = currentTime, key2 = isTimerOn) {
-        if(currentTime > 0 && isTimerOn) {
-            delay(100L)
-            currentTime -= 100L
-            value = currentTime / totalTime.toFloat()
-            currentSec = (currentTime / 1000L).mod(60L)
-            currentMin = (currentTime / 1000L) / 60L
-        }
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
-    ) {
-        Text(
-            text = if (currentSec < 10) {
-                "$currentMin : 0$currentSec"
-            } else {
-                "$currentMin : $currentSec"
-            },
-            fontSize = 44.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-        Button(
-            onClick = {
-                if(currentTime <= 0L) {
-                    currentTime = totalTime
-                    isTimerOn = true
-                } else {
-                    isTimerOn = !isTimerOn
-                }
-            },
-            modifier = Modifier.wrapContentSize(Alignment.Center),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = if (!isTimerOn || currentTime <= 0L) {
-                    Color.Green
-                } else {
-                    Color.Red
-                }
-            )
-        ) {
-            Text(
-                text = if (isTimerOn && currentTime >= 0L) stringResource(id = R.string.stop)
-                else stringResource(id = R.string.start)
-            )
-        }
-    }
-}
-
-@Composable
-fun ToDoScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.teal_700))
-            .wrapContentSize(Alignment.Center)
-    ) {
-        Text(
-            text = "Home Screen",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
     }
 }
---
+++ app/src/main/java/nz/ac/uclive/daa59/topom/Timer.kt
@@ -0,0 +1,81 @@
+package nz.ac.uclive.daa59.topom
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+
+@Composable
+fun TimerScreen(
+    totalTime: Long
+) {
+    var value by remember { mutableStateOf(0f) }
+    var currentTime by remember { mutableStateOf(totalTime) }
+    var currentSec by remember { mutableStateOf(0L) }
+    var currentMin by remember { mutableStateOf(25L) }
+    var isTimerOn by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = currentTime, key2 = isTimerOn) {
+        if(currentTime > 0 && isTimerOn) {
+            delay(100L)
+            currentTime -= 100L
+            value = currentTime / totalTime.toFloat()
+            currentSec = (currentTime / 1000L).mod(60L)
+            currentMin = (currentTime / 1000L) / 60L
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = if (currentSec < 10) {
+                "$currentMin : 0$currentSec"
+            } else {
+                "$currentMin : $currentSec"
+            },
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+        Button(
+            onClick = {
+                if(currentTime <= 0L) {
+                    currentTime = totalTime
+                    isTimerOn = true
+                } else {
+                    isTimerOn = !isTimerOn
+                }
+            },
+            modifier = Modifier.wrapContentSize(Alignment.Center),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = if (!isTimerOn || currentTime <= 0L) {
+                    Color.Green
+                } else {
+                    Color.Red
+                }
+            )
+        ) {
+            Text(
+                text = if (isTimerOn && currentTime >= 0L) stringResource(id = R.string.stop)
+                else stringResource(id = R.string.start)
+            )
+        }
+    }
+}
