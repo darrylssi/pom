639fa7d8a650af708f2079a8ad0da75a47cf1171
Darryl Alang <daa59@uclive.ac.nz>
2023-08-18 05:10:40 +1200

    Adjust height to support landscape

--- app/src/main/java/nz/ac/uclive/daa59/topom/Settings.kt
+++ app/src/main/java/nz/ac/uclive/daa59/topom/Settings.kt
@@ -1,13 +1,18 @@
 package nz.ac.uclive.daa59.topom
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
@@ -43,43 +49,59 @@ fun ThemeComponent (colorViewModel: ColorViewModel) {
     val onChangeState: (String) -> Unit = { selectedValue.value = it }
     val items = listOf("gray", "green", "orange", "pink", "yellow")
 
-    Column(
-        modifier = Modifier.padding(8.dp)
+    val height = LocalConfiguration.current.screenWidthDp.dp
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = stringResource(id = R.string.theme_color),
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-        )
-        items.forEach { item ->
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .selectable(
+        // Wrap the Column with verticalScroll to enable scrolling
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(height)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = stringResource(id = R.string.theme_color),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            items.forEach { item ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .selectable(
+                            selected = isSelectedItem(item),
+                            onClick = {
+                                onChangeState(item)
+                                colorViewModel.selectedOption.value = item
+                                Toast.makeText(contextForToast, toastText, Toast.LENGTH_SHORT)
+                                    .show()
+                            },
+                            role = Role.RadioButton
+                        )
+                        .padding(8.dp)
+                ) {
+                    RadioButton(
                         selected = isSelectedItem(item),
-                        onClick = {
-                            onChangeState(item)
-                            colorViewModel.selectedOption.value = item
-                            Toast.makeText(contextForToast, toastText, Toast.LENGTH_SHORT).show()
-                        },
-                        role = Role.RadioButton
+                        onClick = null
                     )
-                    .padding(8.dp)
-            ) {
-                RadioButton(
-                    selected = isSelectedItem(item),
-                    onClick = null
-                )
-                Text(
-                    text = when (item) {
-                        "gray" -> stringResource(id = R.string.gray)
-                        "orange" -> stringResource(id = R.string.orange)
-                        "yellow" -> stringResource(id = R.string.yellow)
-                        "pink" -> stringResource(id = R.string.pink)
-                        else -> { stringResource(id = R.string.green)}
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                )
+                    Text(
+                        text = when (item) {
+                            "gray" -> stringResource(id = R.string.gray)
+                            "orange" -> stringResource(id = R.string.orange)
+                            "yellow" -> stringResource(id = R.string.yellow)
+                            "pink" -> stringResource(id = R.string.pink)
+                            else -> {
+                                stringResource(id = R.string.green)
+                            }
+                        },
+                        modifier = Modifier.fillMaxWidth().padding(8.dp)
+                    )
+                }
             }
         }
     }
